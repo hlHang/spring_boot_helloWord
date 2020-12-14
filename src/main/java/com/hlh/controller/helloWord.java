7fe2efcd49bf86a068ab17547da4b23a87ee8a7c
hlHang <2710393062@qq.com>
2020-12-14 21:32:32 +0800

    hello Word

---
+++ src/main/java/com/hlh/controller/helloWord.java
@@ -0,0 +1,16 @@
+package com.hlh.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class helloWord {
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String hello() {
+
+        return "helloWord";
+    }
+}
---
+++ src/main/java/com/hlh/startupApplication.java
@@ -0,0 +1,12 @@
+package com.hlh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class startupApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(startupApplication.class,args);
+    }
+}
